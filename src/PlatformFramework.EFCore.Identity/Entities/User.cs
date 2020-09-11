d18e54c12b57bce6d6218e3993e11652b30f45f2
Смирнов Максим Владимирович <max.reborn@gmail.com>
2020-09-11 15:22:28 +0300

    (fix) identity auto-increment Id property

--- src/PlatformFramework.EFCore.Identity/Entities/User.cs
+++ src/PlatformFramework.EFCore.Identity/Entities/User.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlatformFramework.EFCore.Identity.Entities
 {
     public class User : IdentityUser<long>
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override long Id { get; set; }
     }
 }
